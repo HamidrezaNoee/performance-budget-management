151004098bdbb991c3be51bab1f16eb4fa178c46
HamidrezaNoee <132331380+HamidrezaNoee@users.noreply.github.com>
2026-08-28 14:30:42 +0330

    chore(actuals): define ledger SQL precision and field lengths

--- src/PBM.Domain/ActualLedger.cs
+++ src/PBM.Domain/ActualLedger.cs
@@ -1,3 +1,4 @@
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace PBM.Domain;
@@ -18,16 +19,36 @@ public sealed class ActualLedgerEntry : Entity
     public Guid CreatedByUserId { get; set; }
     public Guid? OriginalEntryId { get; set; }
     public ActualLedgerEntryType EntryType { get; set; } = ActualLedgerEntryType.Posting;
+
+    [MaxLength(80)]
     public required string SourceSystem { get; set; }
+
+    [MaxLength(160)]
     public required string ExternalDocumentId { get; set; }
+
+    [MaxLength(160)]
     public required string ExternalLineId { get; set; }
+
+    [MaxLength(64)]
     public required string PayloadHash { get; set; }
+
     public DateTime PostingDate { get; set; }
+
+    [Column(TypeName = "decimal(28,8)")]
     public decimal Amount { get; set; }
+
+    [MaxLength(12)]
     public required string CurrencyCode { get; set; }
+
+    [MaxLength(128)]
     public required string CoordinateHash { get; set; }
+
     public required string CoordinatesJson { get; set; }
+
+    [MaxLength(1000)]
     public string? Note { get; set; }
+
+    [MaxLength(1000)]
     public string? ReversalReason { get; set; }
 
     public Company? Company { get; set; }
