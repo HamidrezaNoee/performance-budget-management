3a366cc0f627dac8fe98ee8dc9599a66e43f02e6
HamidrezaNoee <132331380+HamidrezaNoee@users.noreply.github.com>
2026-08-28 14:10:05 +0330

    Register strategy and KPI objective mapping service

--- src/PBM.Api/ServiceRegistration.cs
+++ src/PBM.Api/ServiceRegistration.cs
@@ -43,6 +43,7 @@ public static class ServiceRegistration
         services.AddScoped<IOrganizationAdminService, OrganizationAdminService>();
         services.AddScoped<IReferenceDataService, ReferenceDataService>();
         services.AddScoped<IKpiService, ScoredKpiService>();
+        services.AddScoped<IStrategyService, StrategyService>();
         services.AddScoped<IPerformanceBudgetingService, PerformanceBudgetingService>();
         services.AddScoped<IAuditService, AuditService>();
         services.AddScoped<IForecastService, ForecastService>();
